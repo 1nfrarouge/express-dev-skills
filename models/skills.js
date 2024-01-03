276bd56d572142aec773dd2545861d767d95dde9
1nfrarouge <samantha.yc.lim@gmail.com>
2024-01-03 17:24:45 -0500

    Refactor Routing

---
+++ models/skills.js
@@ -0,0 +1,13 @@
+const todos = [
+    {id: 1, skill: 'HTML', done: true},
+    {id: 2, skill: 'CSS', done: true},
+    {id: 3, skill: 'JS', done: true}
+  ];
+	
+  module.exports = {
+    getAll
+  };
+	
+  function getAll() {
+    return skills;
+  }
