b19b2e874c5088bf23dceb27f7cf6e94ed9faee6
1nfrarouge <samantha.yc.lim@gmail.com>
2024-01-04 19:16:07 -0500

    Create initial files

--- models/skill.js
+++ models/skill.js
@@ -1,19 +1,24 @@
 const skills = [
-    {id: 1, skill: 'HTML', done: true},
-    {id: 2, skill: 'CSS', done: true},
-    {id: 3, skill: 'Javascript', done: true}
-  ];
-	
-  module.exports = {
-    getAll,
-    getOne
-  };
-	
-  function getAll() {
-    return skills;
-  }
-
-  function getOne(id) {
-    id = parseInt(id);
-    return skills.find(skill => skill.id === id);
-  }
+  {id: 1, skill: 'HTML', done: true},
+  {id: 2, skill: 'CSS', done: true},
+  {id: 3, skill: 'JS', done: true}
+];
+module.exports = {
+  getAll,
+  getOne,
+  create
+};
+function create(skill) {
+  // Add the id
+  skill.id = Date.now() % 1000000;
+  // New skills wouldn't be done :)
+  skill.done = false;
+  skills.push(skill);
+}
+function getAll() {
+  return skills;
+}
+function getOne(id) {
+  id = parseInt(id);
+  return skills.find(skill => skill.id === id);
+}
